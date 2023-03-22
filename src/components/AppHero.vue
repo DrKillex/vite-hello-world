f155129bf45e00af46736ff1bf7b16735a127cf1
DrKillex <lcollaro1994@gmail.com>
2023-03-22 16:58:35 +0100

    completo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data(){
+        return {
+            img: "src/assets/konomorglda.png",
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="img">
+       <img :src="img" alt=""> 
+    </div>
+  
+</template>
+
+<style scoped>
+    .img {
+        height: 500px;
+        max-width: 1000px;
+        margin: 0 auto;
+    }
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data(){
+        return {
+            msg: "La mia prima app con Vite!",
+        }
+    }
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+    h1 {
+        text-align: center;
+        margin-top: 20px;
+    }
+</style>
